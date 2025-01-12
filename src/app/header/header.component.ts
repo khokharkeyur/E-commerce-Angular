cd8c64ba5995c206f1bb03815e58475315bdb793
khokharkeyur <keyurkhokhar04@gmail.com>
2025-01-12 14:49:24 +0530

    dynamic menu rendering based on authentication state

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  menuType: String = 'default';
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.events.subscribe((value: any) => {
+      if (value?.url) {
+        if (localStorage.getItem('seller') && value?.url.includes('seller')) {
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
 }
