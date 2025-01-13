e5da0addab3381496230d7688c45c0c597b5ea55
khokharkeyur <keyurkhokhar04@gmail.com>
2025-01-13 06:21:58 +0530

    Add seller add product feature with routing and header updates

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,33 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, TitleCasePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  menuType: String = 'default';
+  menuType: string = 'default';
+  sellerName: string = '';
   constructor(private router: Router) {}
   ngOnInit(): void {
     this.router.events.subscribe((value: any) => {
       if (value?.url) {
         if (localStorage.getItem('seller') && value?.url.includes('seller')) {
           this.menuType = 'seller';
+          let sellerStore = localStorage.getItem('seller');
+          let sellerData = sellerStore && JSON.parse(sellerStore);
+          this.sellerName = sellerData?.name || '';
         } else {
           this.menuType = 'default';
         }
       }
     });
+  }
+  logout() {
+    localStorage.removeItem('seller');
+    this.router.navigate(['/']);
   }
 }
