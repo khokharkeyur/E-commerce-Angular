0ef2a49031f5020a1b4964d4d8b8c3c5bdf0a844
khokharkeyur <keyurkhokhar04@gmail.com>
2025-01-20 06:11:41 +0530

    Implement product data retrieval in seller update product feature

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Product } from '../interfases';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-update-product',
   imports: [FormsModule],
   templateUrl: './seller-update-product.component.html',
-  styleUrl: './seller-update-product.component.css'
+  styleUrl: './seller-update-product.component.css',
 })
 export class SellerUpdateProductComponent {
-
-  updateProductSubmit(data: Product) {
-
+  route = inject(ActivatedRoute);
+  product = inject(ProductService);
+  productData: Product | undefined;
+  ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.product.getProduct(productId).subscribe((data) => {
+        console.log('data', data);
+        this.productData = data;
+      });
+    }
   }
+
+  updateProductSubmit(data: Product) {}
 }
