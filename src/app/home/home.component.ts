11afc5db8ea7905c8e407172ee473f0ac8a9b07e
khokharkeyur <keyurkhokhar04@gmail.com>
2025-01-22 06:09:38 +0530

    Add Bootstrap and image carousel in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgbCarouselModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
