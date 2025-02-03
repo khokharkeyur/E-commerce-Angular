0d6df3867021981f2d78c34edb7208cdd89f4817
khokharkeyur <keyurkhokhar04@gmail.com>
2025-02-03 06:58:31 +0530

    Add user signup form with styling and functionality

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SignUP } from '../interfases';
 
 @Component({
   selector: 'app-user-auth',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-auth.component.html',
   styleUrl: './user-auth.component.css'
 })
 export class UserAuthComponent {
+
+  signUp(data:SignUP) {
+    console.log('data', data)
+  }
 
 }
