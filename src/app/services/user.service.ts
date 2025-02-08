179efd7a3fa45fd2103d5503d0ee901a1d77186c
khokharkeyur <keyurkhokhar04@gmail.com>
2025-02-08 14:00:50 +0530

    Add user login functionality and update user service for authentication

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { SignUP } from '../interfases';
+import { Login, SignUP } from '../interfases';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
@@ -17,6 +17,27 @@ export class UserService {
         if (response) {
           localStorage.setItem('user', JSON.stringify(response.body));
           this.router.navigate(['/']);
+        }
+      });
+  }
+  userLogin(user: Login) {
+    this.http
+      .get<SignUP[]>(
+        `http://localhost:3000/user?emial=${user.email}&password=${user.password}`,
+        { observe: 'response' }
+      )
+      .subscribe((response) => {
+        console.log('response', response);
+        if (response && response.body) {
+          localStorage.setItem('user', JSON.stringify(response.body[0]));
+          this.router.navigate(['/']);
+          // const userFound = response.find(
+          //   (item: any) => item.email === user.email && item.password === user.password
+          // );
+          // if (userFound) {
+          //   localStorage.setItem('user', JSON.stringify(userFound));
+          //   this.router.navigate(['/']);
+          // }
         }
       });
   }
--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,31 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { SignUP } from '../interfases';
+import { Login, SignUP } from '../interfases';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-auth',
   imports: [FormsModule],
   templateUrl: './user-auth.component.html',
-  styleUrl: './user-auth.component.css'
+  styleUrl: './user-auth.component.css',
 })
 export class UserAuthComponent {
-  user = inject(UserService)
-  showLogin: boolean = false
+  user = inject(UserService);
+  showLogin: boolean = false;
 
   ngOnInit() {
-    this.user.userAuthReaload()
+    this.user.userAuthReaload();
   }
 
-  signUp(data:SignUP) {
-    this.user.userSignUp(data)
+  signUp(data: SignUP) {
+    this.user.userSignUp(data);
   }
 
-  login(data:SignUP) {
-    // this.user.userLogin(data)
+  login(data: Login): void {
+    console.log('caiing', data);
+    this.user.userLogin(data);
   }
-  openLogin() {
+
+  openLogin(): void {
     this.showLogin = !this.showLogin;
   }
-
 }
