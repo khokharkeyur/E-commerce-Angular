38caa20680abd961c24fe7d38cf1f4c5dd617603
khokharkeyur <keyurkhokhar04@gmail.com>
2025-03-23 16:06:47 +0530

    Add checkout component with form for shipping details

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Cart } from '../interfases';
+import { ProductService } from '../services/product.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent {
+  totalPrice: number | undefined;
+  cartData: Cart[] | undefined;
+  orderMsg: string | undefined;
+  constructor(private product: ProductService, private router: Router) {}
 
+  ngOnInit(): void {
+    this.product.currentCart().subscribe((result) => {
+      let price = 0;
+      this.cartData = result;
+      result.forEach((item) => {
+        if (item.quantity) {
+          price = price + +item.price * +item.quantity;
+        }
+      });
+      this.totalPrice = price + price / 10 + 100 - price / 10;
+
+      console.warn(this.totalPrice);
+    });
+  }
 }
